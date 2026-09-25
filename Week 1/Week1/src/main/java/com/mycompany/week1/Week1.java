37f7d29ea48fe4d83154e071e47b8cfd0f158484
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-25 15:55:01 +0300

    init

---
+++ Week 1/Week1/src/main/java/com/mycompany/week1/Week1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week1;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author fatemamaitham
+ */
+public class Week1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Ali");
+        names.add("Mohsen");
+        
+        System.out.println(names);
+    }
+}
